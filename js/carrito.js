4969351bfc97974a69aebfbb5fd0cfcb2abc4245
Jozz16 <jo633554@gmail.com>
2023-01-30 21:56:45 -0300

    cambios casi ginales :c

--- js/carrito.js
+++ js/carrito.js
@@ -3,10 +3,18 @@ const carrito = [
     {id: 2, nombre: 'goma',precio: 200},
     {id: 3, nombre: 'lapiz',precio: 200},
 ]
+let carritoCompleto = []
+
+
 
 function agregarCarro (Producto_id) {
     let carritoFiltrado = carrito.filter(carrito => carrito.id == Producto_id);
+    carritoCompleto.push(carritoFiltrado)
+    console.log(carritoCompleto)
 
-    document.querySelector(".titulo").textContent= 'Nombre del producto:'+' '+ carritoFiltrado[0].nombre; 
-    document.querySelector(".precio").textContent='Precio:'+' '+carritoFiltrado[0].precio;
-}
+    let final = [...carritoFiltrado,...carritoCompleto]
+    console.log(final)
+    document.querySelector(".titulo").textContent= `Nombre del producto: ${final[0].nombre}`; 
+    document.querySelector(".precio").textContent=`Precio:${final[0].precio}`;  
+    
+} 
